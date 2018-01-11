11175b02153970e58990bab8c28262ef1e7a7cd5
Dries Vuylsteke <dvuylsteke@gmail.com>
2018-01-11 11:30:07 +0100

    Create Potion.java

---
+++ OOP/Exam14-15/Potion.java
@@ -0,0 +1,20 @@
+package exam.game;
+
+import be.kuleuven.cs.som.annotate.*;
+
+/** class of potions. */
+public class Potion extends Item {
+	/**
+	 * Check whether this potion can have the given identification as * its
+	 * identification. 
+	 * 
+	 * @return ... 
+	 * 		| (identification > 0) && (identification % 2 == 1)
+	 */
+	@Override
+	@Raw
+	public boolean canHaveAsIdentification(long identification) {
+		return identification > 0 && identification % 2 == 1;
+	}
+
+}
